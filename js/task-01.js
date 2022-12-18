3907e8406ff9e477c54060f7764de301064afe67
owin66 <78900270+owin66@users.noreply.github.com>
2022-12-18 16:45:47 +0200

    task2

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,10 @@
-const listEl = document.querySelector('#categories')
-console.log(listEl)
-const listItem = [...listEl.children]
-console.log(listItem)
-console.log(`Number of categories: ${listItem.length}`)
+const listEl = document.querySelector('#categories');
+console.log(listEl);
+const listItem = [...listEl.children];
+console.log(listItem);
+console.log(`Number of categories: ${listItem.length}`);
 
 listItem.forEach((el, ind) => {
-    console.log(`Category: ${el.firstElementChild.textContent}`)
-    console.log(`Elements: ${el.lastElementChild.children.length}`)
-})
-
-
-
+  console.log(`Category: ${el.firstElementChild.textContent}`);
+  console.log(`Elements: ${el.lastElementChild.children.length}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const makeIngridients = options => {
+  return options.map(option => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('item');
+    liEl.textContent = option;
+    return liEl;
+  });
+};
+
+const elements = makeIngridients(ingredients);
+
+ingredientsList.append(...elements);
